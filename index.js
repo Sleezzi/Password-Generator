15ba4e4381c242eee7cdd7f2fb8004d2b873cf57
Sleezzi <ilan.dfp91@gmail.com>
2024-08-03 23:49:45 +0200

    Correction des bugs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const text = "Easily generate random passwords in your browser";
+const text = "Easily generate random passwords in your browser";
 let i = 0;
 document.querySelector("#typingContainer h1").innerText = "";
 function typeWriter() {
     if (i < text.length) {
-        document.querySelector("#typingContainer h1").innerText += text.charAt(i);
+        document.querySelector("#typingContainer h1").innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter, 50);
     }
